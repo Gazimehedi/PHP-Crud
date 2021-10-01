d01419fadfb58223ced522623154bc2d558667ef
Gazimehedi <gazimehedihasan512@gmail.com>
2021-10-01 23:33:09 +0600

    delete data with image

--- delete.php
+++ delete.php
@@ -1,7 +1,11 @@
 <?php
 include "config.php";
 $id = $_REQUEST['id'];
-
+$sql0 = "SELECT * FROM user WHERE id='$id'";
+$query = mysqli_query($conn, $sql0);
+$data = mysqli_fetch_assoc($query);
+$path = "Upload/".$data['image'];
+unlink($path);
 $sql = "DELETE FROM user WHERE id='$id'";
 if(mysqli_query($conn,$sql)){
     header("location: /PHP-Crud/read.php");
